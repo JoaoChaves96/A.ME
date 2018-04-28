fdd68fb59357257a8edde2dfe4523f1ad91d6f56
joao <joao@wifi-interno.fe.up.pt>
2018-04-28 18:24:24 +0100

    created home page with default video

--- adoptme/src/App.vue
+++ adoptme/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <el-header>
+      <img id="logo" src="./assets/new_logo.png">
+    </el-header>
     <router-view/>
   </div>
 </template>
@@ -16,8 +18,10 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+  #logo {
+    width: 100%;
+  }
 </style>
---
+++ adoptme/src/components/Home.vue
@@ -0,0 +1,56 @@
+<template class="hello">
+  <div>
+    <div class="video">
+      <video autoplay muted loop>
+        <source src="../assets/home.mp4" type="video/mp4">
+      </video>
+    </div>
+    <div>
+      <el-button id="btn">Entrar</el-button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .video {
+    position:fixed;
+    right: 0;
+    margin:0;
+    width:100%;
+    height:100%;
+    overflow: hidden;
+  }
+  .video video {
+    position: absolute;
+    width: 100% !important;
+    height: auto !important;
+    background-size: cover;
+    transition: 1s opacity;
+    z-index:-1;
+  }
+
+  #btn {
+    width:10%;
+    position: relative;
+    margin-top: 30%;
+    margin-left: 45%;
+    z-index: 1000;
+    background-color: gold;
+    text-align: center;
+    color: black;
+    border: none;
+  }
+</style>
